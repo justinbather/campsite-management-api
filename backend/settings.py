62472e3743c0cad65f4aae33f1f47739be3b8835
justinbather <justinbather@hotmail.com>
2023-09-02 15:30:36 -0400

    add static root

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/', 'static/')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'assets/site-images/'
