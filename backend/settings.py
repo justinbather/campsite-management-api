cd9e370b010843cab8cb8e6d973f3193bb6cc7bd
justinbather <justinbather@hotmail.com>
2023-09-05 11:43:43 -0400

    changed static file root

--- backend/settings.py
+++ backend/settings.py
@@ -91,6 +91,7 @@ DB_PASS = os.environ.get('DB_PASS')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -101,15 +102,14 @@ DATABASES = {
         'PORT': DB_PORT,
     }
 }
-"""
-DATABASES = {
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 """
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -146,11 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets', 'static')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/', 'static/')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'assets/site-images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 # Default primary key field type
