e876902d4e08dba265091096b7609509a17a7645
justinbather <justinbather@hotmail.com>
2023-09-05 11:48:47 -0400

    changed static file root

--- backend/settings.py
+++ backend/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets', 'static')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/', 'static/')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'assets/site-images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_ROOT = 'assets/'
 
 
 # Default primary key field type
