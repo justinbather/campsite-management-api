36285a4035859769da6535d888c598d90d322358
justinbather <justinbather@hotmail.com>
2023-09-05 16:00:03 -0400

    default file storage

--- backend/settings.py
+++ backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
 }
 """
 
-STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
