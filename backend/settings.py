1d8b02bb4313162adba230826f04f5ef12071887
justinbather <justinbather@hotmail.com>
2023-09-05 15:49:53 -0400

    added aws s3 file storage

--- backend/settings.py
+++ backend/settings.py
@@ -111,6 +111,13 @@ DATABASES = {
 }
 """
 
+STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
