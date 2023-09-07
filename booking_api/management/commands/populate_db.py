afd691f4e363c17a4f42781f9c63a3226220c118
justinbather <justinbather@hotmail.com>
2023-09-07 18:30:14 -0400

    add script to populate db

---
+++ booking_api/management/commands/populate_db.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from booking_api.models import *
+
+class Command(BaseCommand):
+
+    def _create_sites(self):
+        site_obj = Site.objects.get(id=1)
+        site_types = ['Tent', 'Pull Through', 'Back In']
+
+        for i in 15:
+            for type in site_types:
+                site_obj.id = None
+                site_obj._state.adding = True
+                site_obj.site_type = type
+                site_obj.save()
+        
+
+    def handle(self, *args, **options):
+        self._create_sites()
