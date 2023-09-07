0790c211a5e384977ce12e0116300a920597149f
justinbather <justinbather@hotmail.com>
2023-09-07 18:35:42 -0400

    add script to populate db

--- booking_api/management/commands/populate_db.py
+++ booking_api/management/commands/populate_db.py
@@ -7,12 +7,14 @@ class Command(BaseCommand):
         site_obj = Site.objects.get(id=1)
         site_types = ['Tent', 'Pull Through', 'Back In']
 
-        for i in 15:
+        for i in range(15):
             for type in site_types:
                 site_obj.id = None
                 site_obj._state.adding = True
                 site_obj.site_type = type
+
                 site_obj.save()
+                print("Created Site Object")
         
 
     def handle(self, *args, **options):
